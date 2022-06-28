050879b0e08b1af6e6f712eb6bdc3aa8a7ca4bde
Adam <adamguzzo@cox.net>
2022-06-28 14:55:38 -0500

    Testing more functions using JavaScript

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ function revealMessage()
 {
 	document.getElementById("hiddenMessage").style.display = 'block';
 }
+function countUp()
+{
+	var currentVal = document.getElementById("counterText").innerHTML;
+	var newVal = currentval + 1;
+	document.getElementById("counterText").innerHTML = newVal;
+}
+function countDown()
+{
+	var currentVal = document.getElementById("counterText").innerHTML;
+	var newVal = currentVal - 1;
+	document.getElementById("counterText").innerHTML = newVal;
+}
