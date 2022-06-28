54ae5861af24c9eda2dba681105626b2e02f8231
Adam <adamguzzo@cox.net>
2022-06-28 15:25:08 -0500

    Bug Fixes
    Fixed bug where increment button retrieved value as string and processed as such

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ function revealMessage()
 }
 function countUp()
 {
-	var currentVal = document.getElementById("counterText").innerHTML;
+	var currentVal = parseInt(document.getElementById("counterText").innerHTML);
 	var newVal = currentVal + 1;
 	document.getElementById("counterText").innerHTML = newVal;
 }
 function countDown()
 {
-	var currentVal = document.getElementById("counterText").innerHTML;
+	var currentVal = parseInt(document.getElementById("counterText").innerHTML);
 	var newVal = currentVal - 1;
 	document.getElementById("counterText").innerHTML = newVal;
 }
